0b60bd5db669f4fd94b1bcd7550afa6491eb3ed9
warrenson <warrenson@github.com>
2026-02-20 00:08:28 +1300

    auto-formatting

--- src/file.c
+++ src/file.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
 
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
 
-#include "file.h"
 #include "common.h"
-
+#include "file.h"
 
 int create_db_file(char *filename) {
+  int fd = open(filename, O_RDWR | O_CREAT, 06444);
+  if (fd == -1) {
+    perror("open");
+    return STATUS_ERROR;
+  }
 
+  return 0;
 }
 
-int open_db_file(char *filename) {
-
-}
-
-
+int open_db_file(char *filename) { return 0; }
--- src/main.c
+++ src/main.c
@@ -1,6 +1,6 @@
-#include <stdio.h>
-#include <stdbool.h>
 #include <getopt.h>
+#include <stdbool.h>
+#include <stdio.h>
 #include <stdlib.h>
 
 #include "common.h"
@@ -8,8 +8,48 @@
 #include "parse.h"
 
 void print_usage(char *argv[]) {
+    printf("Usage: %s -n -f <database file>\n", argv[0]);
+    printf("\t -n            - create new database file\n");
+    printf("\t -f <filepath> - (required) path to database file\n");
+    return;
 }
 
-int main(int argc, char *argv[]) { 
-	
+int main(int argc, char *argv[]) {
+    char *filepath = NULL;
+    char *author = NULL;
+    bool newfile = false;
+    int c;
+
+    int dbfd = -1;
+
+    while ((c = getopt(argc, argv, "nf:")) != -1) {
+        switch (c) {
+        case 'n':
+            newfile = true;
+            break;
+        case 'f':
+            filepath = optarg;
+        case '?':
+            printf("Unknown option -%c\n", c);
+            print_usage(argv);
+            return 0;
+        default:
+            return -1;
+        }
+    }
+
+    if (filepath == NULL) {
+        printf("Filepath argument is required!\n");
+        print_usage(argv);
+        return 0;
+    }
+
+    if (newfile) {
+        dbfd = create_db_file(filepath);
+    }
+
+    printf("Newfile: %d\n", newfile);
+    printf("Filepath: %s\n", filepath);
+
+    return 0;
 }
