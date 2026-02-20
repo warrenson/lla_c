26cca8cca1babb9a99644d90329c4f4fb8e10668
warrenson <warrenson@github.com>
2026-02-20 21:55:20 +1300

    implement create_db_file open_db_file fns

--- src/file.c
+++ src/file.c
@@ -9,13 +9,26 @@
 #include "file.h"
 
 int create_db_file(char *filename) {
-  int fd = open(filename, O_RDWR | O_CREAT, 06444);
-  if (fd == -1) {
-    perror("open");
-    return STATUS_ERROR;
-  }
+    int fd = open(filename, O_RDONLY);
+    if (fd != -1) {
+        close(fd);
+        printf("File already exists!!!\n");
+        return STATUS_ERROR;
+    }
 
-  return 0;
+    fd = open(filename, O_RDWR | O_CREAT, 0644);
+    if (fd == -1) {
+        perror("open");
+        return STATUS_ERROR;
+    }
+    return fd;
 }
 
-int open_db_file(char *filename) { return 0; }
+int open_db_file(char *filename) {
+    int fd = open(filename, O_RDWR, 06444);
+    if (fd == -1) {
+        perror("open");
+        return STATUS_ERROR;
+    }
+    return fd;
+}
