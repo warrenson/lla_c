c182a9370e516d4f7a7a98d76e6c5472285419cd
warrenson <warrenson@github.com>
2026-02-23 23:47:24 +1300

    comment out code unused code at this point

--- src/parse.c
+++ src/parse.c
@@ -10,13 +10,13 @@
 #include "common.h"
 #include "parse.h"
 
-void list_employees(struct dbheader_t *dbhdr, struct employee_t *employees) {}
+// void list_employees(struct dbheader_t *dbhdr, struct employee_t *employees) {}
 
-int add_employee(struct dbheader_t *dbhdr, struct employee_t *employees,
-                 char *addstring) {}
+// int add_employee(struct dbheader_t *dbhdr, struct employee_t *employees,
+                 // char *addstring) {}
 
-int read_employees(int fd, struct dbheader_t *dbhdr,
-                   struct employee_t **employeesOut) {}
+// int read_employees(int fd, struct dbheader_t *dbhdr,
+                   // struct employee_t **employeesOut) {}
 
 int output_file(int fd, struct dbheader_t *dbhdr,
                 struct employee_t *employees) {
