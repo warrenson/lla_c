dfdb25385f091452e6d1c13d52a431d6b4fb8f65
warrenson <warrenson@github.com>
2026-03-01 16:10:17 +1300

    list_employess returns int status

--- include/parse.h
+++ include/parse.h
@@ -23,7 +23,7 @@ int validate_db_header(int fd, struct dbheader_t **headerOut);
 int read_employees(int fd, struct dbheader_t *,
                    struct employee_t **employeesOut);
 int output_file(int fd, struct dbheader_t *, struct employee_t *employees);
-void list_employees(struct dbheader_t *dbhdr, struct employee_t *employees);
+int list_employees(struct dbheader_t *dbhdr, struct employee_t *employees);
 int add_employee(struct dbheader_t *dbhdr, struct employee_t **employees,
                  char *addstring);
 
--- src/parse.c
+++ src/parse.c
@@ -10,7 +10,7 @@
 #include "common.h"
 #include "parse.h"
 
-void list_employees(struct dbheader_t *dbhdr, struct employee_t *employees) {
+int list_employees(struct dbheader_t *dbhdr, struct employee_t *employees) {
     int i = 0;
     printf("%-32s%-32s%-5s\n", "NAME", "ADDRESS", "HOURS");
     for (; i < dbhdr->count; i++) {
@@ -19,6 +19,7 @@ void list_employees(struct dbheader_t *dbhdr, struct employee_t *employees) {
         printf("\tAddress: %s\n", employees[i].address);
         printf("\tHours: %u\n", employees[i].hours);
     }
+    return STATUS_SUCCESS;
 }
 
 int add_employee(struct dbheader_t *dbhdr, struct employee_t **employees,
